887a48d770ed576c6e1d011b5585d1210e41c9ab
DevBlack <nyrocha018@gmail.com>
2022-07-30 20:39:21 -0300

    testing traces

--- app/src/main/java/com/devblack/xray/infra/XRayTrace.java
+++ app/src/main/java/com/devblack/xray/infra/XRayTrace.java
@@ -4,13 +4,16 @@ package com.devblack.xray.infra;
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.entities.Segment;
 import com.amazonaws.xray.entities.Subsegment;
+import com.amazonaws.xray.entities.TraceID;
 
 public class XRayTrace {
 	
 	public static void trace(final String traceId, final Runnable runnable) {
 		
 		final Subsegment subsegment = AWSXRay.beginSubsegment(traceId);
+		subsegment.setTraceId(TraceID.fromString(traceId));
 		final Segment segment = AWSXRay.beginSegment(traceId);
+		segment.setTraceId(TraceID.fromString(traceId));
 		
 		try {
 			runnable.run();
@@ -19,8 +22,8 @@ public class XRayTrace {
 			segment.addException(e);
 			throw e;
 		} finally {
-			AWSXRay.endSubsegment();
 			AWSXRay.endSegment();
+			AWSXRay.endSubsegment();
 		}
 	}
 	
