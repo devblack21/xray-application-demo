e0021c0bb73b61f8be61be0825c7da12ae11a788
DevBlack <nyrocha018@gmail.com>
2022-07-30 18:33:34 -0300

    refact: app.sh, memory and cpu values

--- app/app.sh
+++ app/app.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xms${JVM_XMS} -Xmx${JVM_XMX} -d64 -XX:+USeG1GC -noverify -jar app.jar --server.port=${PORT}
+java -Xms${JVM_XMS} -Xmx${JVM_XMX} -d64 -noverify -jar app.jar --server.port=${PORT}
