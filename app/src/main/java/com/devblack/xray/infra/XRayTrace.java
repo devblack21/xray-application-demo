7bd8f4375bebcb6ce07a400554ff2b2e15af2393
DevBlack <nyrocha018@gmail.com>
2022-07-30 19:04:48 -0300

    feat: testing segment

--- app/src/main/java/com/devblack/xray/infra/XRayTrace.java
+++ app/src/main/java/com/devblack/xray/infra/XRayTrace.java
@@ -2,20 +2,20 @@ package com.devblack.xray.infra;
 
 
 import com.amazonaws.xray.AWSXRay;
-import com.amazonaws.xray.entities.Subsegment;
+import com.amazonaws.xray.entities.Segment;
 
 public class XRayTrace {
 	
 	public static void trace(final String traceId, final Runnable runnable) {
 		
-		final Subsegment subsegment = AWSXRay.beginSubsegment(traceId);
+		final Segment subsegment = AWSXRay.beginSegment(traceId);
 		try {
 			runnable.run();
 		} catch (final Exception e) {
 			subsegment.addException(e);
 			throw e;
 		} finally {
-			AWSXRay.endSubsegment();
+			AWSXRay.endSegment();
 		}
 	}
 	
